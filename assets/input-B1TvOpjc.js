6bda650ceec8bbecd289d017bb08a641e8d6f184
unknown <ozoghayyer11@gmail.com>
2026-05-08 22:36:01 -0700

    Updates

---
+++ assets/input-B1TvOpjc.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CytdjC8G.js";function e({className:r="",...o}){return n.jsx("input",{...o,className:["w-full h-10 px-3 rounded-md border border-gray-300 bg-white text-sm focus:ring-2 focus:ring-[var(--primary)] focus:border-[var(--primary)] outline-none",r].join(" ")})}export{e as I};
